90298a421797e49b5aabc31aa0066c02c586d81b
vedelopment <vedelopment@gmail.com>
2017-01-09 11:32:26 -0800

    saves changes before switching branches

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,10 +8,34 @@ angular
   function AlbumsIndexController (  $http  ) {
   var vm = this;
   vm.results = null;
+  // vm.newAlbum = {
+  //     name: 'License to Ill',
+  //     artistName: 'Beastie Boys'
+  // };
+
+  // vm.albums = [
+  // ];
 
   $http({
     method: 'GET',
     url: '/api/albums'
+  }).then(resSuccess, resError);
+
+  function resSuccess(res) {
+    console.log("resSuccess:", res);
+    vm.albums = res.data;
+  }
+
+  function resError(res) {
+    console.log("error:" + res);
+  }
+
+  $http({
+    method: 'POST',
+    url: '/api/albums'
+    data: {
+
+    }
   }).then(resSuccess, resError);
 
   function resSuccess(res) {
